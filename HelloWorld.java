73cb33e9d24d4c4fd1109851303293b5e7d8602f
siri <sirishat.dwh@gmail.com>
2020-04-12 19:45:19 +0530

    added new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello, sirisha");
+    System.out.println("Hello, sirisa");
+    System.out.println("Hello, this is devops class");
+    System.out.println("Hello, kaushik");
   }
 }
